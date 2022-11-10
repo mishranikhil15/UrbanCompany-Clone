844fc87bec37e63ab97b38998c6e34e2a9d1547a
Nikhil Kumar <vashishtnikhil158@gmail.com>
2022-11-10 23:05:16 +0530

    initial

---
+++ script1.js
@@ -0,0 +1,10 @@
+// let change=document.querySelector("#select").value;
+// change.addEventListener("change",changecity);
+
+function changecity() {
+    let change = document.querySelector("#new").value;
+    window.location.href = "newdelhi.html";
+    console.log(change);
+}
+// window.location.href = "newdelhi.html";
+
